078f87677392e85137285b58c5db3462d7563eae
shan33 <sh43204327@gmail.com>
2021-05-26 08:22:18 +0800

    fix: 修復 Discount 與 DiscountTest 誤用時間萃取方法

--- src/main/java/main/Discount.java
+++ src/main/java/main/Discount.java
@@ -1,44 +1,31 @@
 package main;
 
 public class Discount {
-	private static int general = 200;
-	private static int weekend = 250;
-	private int money;
-	private float discount = 1;
+	private float discount = 0;
 	
-	public Discount(Identity identity) {
-		
-		if (identity.getWeek() >= 1 && identity.getWeek() <= 5) {
-			money = general;
-		} else if (identity.getWeek() == 6 || identity.getWeek() == 7) {
-			money = weekend;
-		}
+	public Discount(Identity identity, String dateTime) {
 
-		if (identity.isMember()) {
-			discount = (float) 0.5;
-		} else if (identity.isGroup()) {
-			discount = (float) 0.7;
-		} else if (identity.getAge() < 12 || identity.getAge() >= 60) {
-			discount = (float) 0.8;
-		} else if (identity.getHour() >= 5 && identity.getHour() < 7) {
-			discount = (float) 0.8;
+		int hour = InputNormalization.extractHour(dateTime);
+		int min = InputNormalization.extractMin(dateTime);
+
+		if (hour < 5 || (hour == 22 && min > 0) || hour > 22) {
+			System.out.println("Business hours: 05:00-22:00");
 		} else {
-			discount = 1;
+			if (identity.isMember()) {
+				discount = (float) 0.5;
+			} else if (identity.isGroup()) {
+				discount = (float) 0.7;
+			} else if (identity.getAge() < 12 || identity.getAge() >= 60) {
+				discount = (float) 0.8;
+			} else if (hour >= 5 && hour < 7) {
+				discount = (float) 0.8;
+			} else {
+				discount = 1;
+			}
 		}
-		
-		System.out.println("Money: " +  money);
-		System.out.println("Discount: " + discount);
 	}
 
 	public float getDiscount() {
 		return discount;
-	}
-	
-	public int getMoney() {
-		return money;
-	}
-	
-	public float getTotal() {
-		return discount * money;
 	}
 }
--- src/test/java/main/DiscountTest.java
+++ src/test/java/main/DiscountTest.java
@@ -1,48 +1,60 @@
 package main;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiscountTest {
 
+	static String dateTime;
+
+	@BeforeEach
+	public void catchCurrentTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd E HH:mm:ss");
+		dateTime = sdf.format(new Date());
+	}
+
 	@Test
 	public void testAgeHasDiscout() {
 		Identity identity = new Identity(10, false, false);
-		Discount discount = new Discount(identity);
+		Discount discount = new Discount(identity, dateTime);
 		
-		float expected = (float) (200 * 0.8);
-		float result = discount.getTotal();
+		float expected = (float) 0.8;
+		float result = discount.getDiscount();
 		assertEquals(expected, result);
 	}
 	
 	@Test	
 	public void testAgeHasNoDiscout() {
 		Identity identity = new Identity(40, false, false);
-		Discount discount = new Discount(identity);
+		Discount discount = new Discount(identity, dateTime);
 		
-		float expected = (float) (250 * 0.8);
-		float result = discount.getTotal();
+		float expected = (float) 0.8;
+		float result = discount.getDiscount();
 		assertEquals(expected, result);
 	}
 	
 	@Test
 	public void testAgeLessThan() {
 		Identity identity = new Identity(2, false, true);
-		Discount discount = new Discount(identity);
+		Discount discount = new Discount(identity, dateTime);
 		
 		float expected = (float) (0);
-		float result = discount.getTotal();
+		float result = discount.getDiscount();
 		assertEquals(expected, result);
 	}
 	
 	@Test
 	public void testAgeMoreThan() {
 		Identity identity = new Identity(80, true, false);
-		Discount discount = new Discount(identity);
+		Discount discount = new Discount(identity, dateTime);
 		
 		float expected = (float) (0);
-		float result = discount.getTotal();
+		float result = discount.getDiscount();
 		assertEquals(expected, result);
 	}
 }

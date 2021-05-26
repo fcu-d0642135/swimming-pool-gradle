10ba1b006b46d12f65dbca5d2cd8260a3f3eefd1
shan33 <sh43204327@gmail.com>
2021-05-26 15:08:06 +0800

    test: 增加營業與非營業時間折扣情況的測試

--- src/test/java/main/DiscountTest.java
+++ src/test/java/main/DiscountTest.java
@@ -1,46 +1,66 @@
 package main;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class DiscountTest {
 
-	static String dateTime;
+	@DisplayName("不同年紀的折扣")
+	@Nested
+	class DifferentAges {
+		String dateTime = "2021-05-26 週三 14:30:00";
 
-	@BeforeEach
-	public void catchCurrentTime() {
-//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd E HH:mm:ss");
-//		dateTime = sdf.format(new Date());
-		dateTime = "2021-05-26 週三 14:30:00";
+		@Test
+		public void testAgeHasDiscount() {
+			Identity identity = new Identity(10, false, false);
+			Discount discount = new Discount(identity, dateTime);
+			System.out.println(discount.getDiscount());
+			Assertions.assertEquals(0.8, discount.getDiscount());
+		}
+
+		@Test
+		public void testAgeHasNoDiscount() {
+			Identity identity = new Identity(40, false, false);
+			Discount discount = new Discount(identity, dateTime);
+			Assertions.assertEquals(1, discount.getDiscount());
+		}
+
+		@Test
+		public void testAgeLessThan() {
+			Identity identity = new Identity(2, false, true);
+			Discount discount = new Discount(identity, dateTime);
+			Assertions.assertEquals(0, discount.getDiscount());
+		}
+
+		@Test
+		public void testAgeMoreThan() {
+			Identity identity = new Identity(80, true, false);
+			Discount discount = new Discount(identity, dateTime);
+			Assertions.assertEquals(0, discount.getDiscount());
+		}
 	}
 
-	@Test
-	public void testAgeHasDiscount() {
-		Identity identity = new Identity(10, false, false);
-		Discount discount = new Discount(identity, dateTime);
-		System.out.println(discount.getDiscount());
-		Assertions.assertEquals(0.8, discount.getDiscount());
-	}
-	
-	@Test	
-	public void testAgeHasNoDiscount() {
-		Identity identity = new Identity(40, false, false);
-		Discount discount = new Discount(identity, dateTime);
-		Assertions.assertEquals(1, discount.getDiscount());
-	}
-	
-	@Test
-	public void testAgeLessThan() {
-		Identity identity = new Identity(2, false, true);
-		Discount discount = new Discount(identity, dateTime);
-		Assertions.assertEquals(0, discount.getDiscount());
-	}
-	
-	@Test
-	public void testAgeMoreThan() {
-		Identity identity = new Identity(80, true, false);
-		Discount discount = new Discount(identity, dateTime);
-		Assertions.assertEquals(0, discount.getDiscount());
+	@DisplayName("營業與非營業時間")
+	@Nested
+	class DifferentBusinessHours {
+		@Test
+		public void testNoBusiness() {
+			Identity identity = new Identity(25, false, false);
+			Discount discount = new Discount(identity, "2021-05-26 週三 04:30:00");
+			Assertions.assertEquals(0, discount.getDiscount());
+		}
+
+		@Test
+		public void testEarlyBirdBusiness() {
+			Identity identity = new Identity(25, false, false);
+			Discount discount = new Discount(identity, "2021-05-26 週三 06:30:00");
+			Assertions.assertEquals(0.8, discount.getDiscount());
+		}
+
+		@Test
+		public void testHaveBusiness() {
+			Identity identity = new Identity(25, false, false);
+			Discount discount = new Discount(identity, "2021-05-26 週三 10:30:00");
+			Assertions.assertEquals(1, discount.getDiscount());
+		}
 	}
 }
